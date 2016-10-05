e912eb2e09190a3f34c6f0cf34ff9df42bbd7c80
Dan Harper <intouch@danharper.me>
2016-10-06 00:42:52 +0100

    x

--- src/App.js
+++ src/App.js
@@ -271,10 +271,18 @@ class App extends Component {
 
 	previous = { code: '', evaluated: '' }
 
+	get presets() {
+		return Object.keys(this.state.presets).filter(k => this.state.presets[k])
+	}
+
+	get plugins() {
+		return Object.keys(this.state.plugins).filter(k => this.state.plugins[k])
+	}
+
 	get transformed() {
 		try {
-			const presets = Object.keys(this.state.presets).filter(k => this.state.presets[k])
-			const plugins = Object.keys(this.state.plugins).filter(k => this.state.plugins[k])
+			const presets = this.presets
+			const plugins = this.plugins
 
 			const code = Babel.transform(this.state.code, { presets, plugins }).code
 			const evaluated = this.evaluate(code)
@@ -337,6 +345,70 @@ class App extends Component {
 		done = true
 
 		return buffer.join('\n')
+	}
+
+	xcomponentWillMount() {
+		const lsState = this.decodeStorage() || {}
+		const urlState = this.parseQuery() || {}
+
+		this.setState({
+			babili: false,
+			evaluate: true,
+			lineWrap: false,
+			presets: { es2015: true, 'stage-2': true, react: true },
+			...lsState,
+			...urlState
+		})
+	}
+
+	decodeStorage() {
+		try {
+			return JSON.parse(window.localStorage.getItem('replState'))
+		} catch (e) {
+			return null
+		}
+	}
+
+	parseQuery() {
+		const query = window.location.hash.replace(/^\#\?/, '')
+
+		if (!query) return null
+
+		return query.split('&').map(param => {
+			const splitPoint = param.indexOf('=')
+			return {
+				key: param.substring(0, splitPoint),
+				value: param.substring(splitPoint + 1)
+			}
+		}).reduce((carry, { key, value }) => {
+			if (key && value) {
+				try {
+					carry[key] = window.decodeURIComponent(String(value))
+				} catch (e) {}
+			}
+		})
+	}
+
+	componentDidUpdate() {
+		const state = {
+			code: this.state.code,
+			presets: this.presets,
+			babili: false, // TODO
+			lineWrap: false, // TODO
+			evaluate: false, // TODO
+		}
+
+		try {
+			window.localStorage.setItem('replState', JSON.stringify(state))
+		} catch (e) {}
+
+		// TODO: handle presets better here
+		// existing code does [a,b,c], we need { a: true, b: true, c: true }
+		const query = Object.keys(state)
+			.map(key => `${key}=${window.encodeURIComponent(state[key])}`)
+			.join('&')
+
+		window.location.hash = '?' + query
 	}
 }
 
