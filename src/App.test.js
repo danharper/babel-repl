29f12b8fef408da49da4c666022968c48f545591
Moti Zilberman <motiz88@gmail.com>
2016-10-20 13:26:19 +0300

    Mount div to document for testing
    For some reason, react-ace uses passes an ID to brace instead of a DOM element reference, so it expects to be mounted in an actual document.

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+describe("App", () => {
+  let div;
+
+  beforeEach(() => {
+    div = document.createElement("div");
+    document.body.appendChild(div);
+  });
+
+  it("renders without crashing", () => {
+    ReactDOM.render(<App />, div);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(div);
+  });
 });
